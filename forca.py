7fd91f9532454223fe33a98b99f90ce71a64051f
Marcos <marcosvdiasaraujo2@gmail.com>
2022-11-30 21:48:10 -0300

    #Adicionado as funções IF E ELSE para adicionar erro de tentativas ao jogo da forca

--- forca.py
+++ forca.py
@@ -4,28 +4,37 @@ def jogar_forca():
     print("Bem vindo ao jogo da forca")
     print ("========================")
 
-palavra_secreta = "palavra"
-letras_acertadas = ["_", "_", "_", "_", "_", "_", "_"]
-enforcou = False
-acertou = False
-
-#Enquanto o False for verdade(true) o While irá rodar
-while(not enforcou and not acertou):
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_", "_", "_", "_", "_", "_",]
     
-    print(letras_acertadas)
-    chute = input("Qual é a palavra?" )
-    chute = chute.strip()  #Ignora os espaços no input ex: '  A  '
-
-    index = 0
-    for letra in palavra_secreta:  
-        if (chute.upper() == letra.upper()):
-            letras_acertadas[index] = letra
-        index = index + 1
+    enforcou = False
+    acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
+    #Enquanto o False for verdade(true) o While irá rodar
+    
+    while(not acertou and not enforcou):
+        
+        chute = input("Qual é a letra?" )
+        chute = chute.strip().upper()  #Ignora os espaços no input ex: '  A  '
+        
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:  
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
-print("Fim do jogo")
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas #esqueci de colocar o "not" então o laço não se formou
+        print(letras_acertadas)
+
+
+    print("Fim do jogo")
 
 if(__name__ == "__main__"):
     jogar_forca()

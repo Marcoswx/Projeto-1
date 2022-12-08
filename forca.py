774123fa8c7f098c621486646e3a9563f1c3a342
Marcos <marcosvdiasaraujo2@gmail.com>
2022-12-07 21:39:12 -0300

    Criação das funções

--- forca.py
+++ forca.py
@@ -1,40 +1,80 @@
+import random
 
 def jogar_forca():
-    print ("========================")
-    print("Bem vindo ao jogo da forca")
-    print ("========================")
-
-    palavra_secreta = "banana".upper()
-    letras_acertadas = ["_", "_", "_", "_", "_", "_",]
     
+    imprime_mensagem_inicio()
+    palavra_secreta = carrega_palavra_secreta()
+    
+    letras_acertadas = substituição_acertos(palavra_secreta)
+    print(letras_acertadas)
+
     enforcou = False
     acertou = False
     erros = 0
 
-    print(letras_acertadas)
-
     #Enquanto o False for verdade(true) o While irá rodar
     
-    while(not acertou and not enforcou):
+    while(not enforcou and not acertou):
         
-        chute = input("Qual é a letra?" )
-        chute = chute.strip().upper()  #Ignora os espaços no input ex: '  A  '
-        
-        if(chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:  
-                if (chute == letra):
-                    letras_acertadas[index] = letra
-                index += 1
-        else:
-            erros += 1
+        chute = pede_chute()
 
+        if(chute in palavra_secreta):
+            marca_chute_correto(chute, letras_acertadas, palavra_secreta)
+        else:
+            erros += 1 
+            
         enforcou = erros == 6
-        acertou = "_" not in letras_acertadas #esqueci de colocar o "not" então o laço não se formou
+        acertou = "_" not in letras_acertadas 
+        
         print(letras_acertadas)
 
+    if(acertou):
+        imprime_mensagem_vencedor()
+    else:
+        imprime_mensagem_perdedor()
 
-    print("Fim do jogo")
+def imprime_mensagem_inicio():
+
+    print ("========================")
+    print("Bem vindo ao jogo da forca")
+    print ("========================")
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+    
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+def substituição_acertos(palavra):
+    return ["_" for letra in palavra]
+
+def pede_chute():
+    chute = input("Qual é a letra?" )
+    chute = chute.strip().upper()  #Ignora os espaços no input ex: '  A  '
+    
+    return chute
+
+def marca_chute_correto(chute,letras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:  
+        if (chute == letra):
+            letras_acertadas[index] = letra
+        index += 1  
+
+def imprime_mensagem_vencedor():
+    print("Parabens você ganhou!")
+
+def imprime_mensagem_perdedor():
+    print("Que pena você perdeu!")
+
 
 if(__name__ == "__main__"):
     jogar_forca()
